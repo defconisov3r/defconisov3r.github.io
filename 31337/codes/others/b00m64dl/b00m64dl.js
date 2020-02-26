13637009bd382ba8942657530405f14971893759
ㅤ <7275589+defconisov3r@users.noreply.github.com>
2020-02-26 12:30:59 -0600

    Update b00m64dl.js

--- 31337/codes/others/b00m64dl/b00m64dl.js
+++ 31337/codes/others/b00m64dl/b00m64dl.js
@@ -1,8 +1,8 @@
 i=0;
 while(i<5){
   a=document.createElement('iframe');
-  b=a.src;
   b="data:application/octet-stream;base64,YjAwbTY0ZGwK";
+  a.src=b;
   c=document.body;
   c.appendChild(a);
   i++;
