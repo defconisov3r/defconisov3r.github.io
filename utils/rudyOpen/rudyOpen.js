08e18c0402713566f35f35f985a56ff387ae2a6f
ㅤ <7275589+defconisov3r@users.noreply.github.com>
2020-10-21 20:03:10 +0000

    Create rudyOpen.js

---
+++ utils/rudyOpen/rudyOpen.js
@@ -0,0 +1,20 @@
+target=document.getElementById("rudyTargets").value.split(",");
+console.log(target);
+l=target.length;
+targetActive=null;
+i=0;
+function rudyOpen(url){
+	targetActive=window.open(url,'_blank');
+	if(targetActive==null){
+		return true;
+	}
+	return false;
+}
+function runRudyOpen(){	
+	if(!(targetActive==null)){
+		domain="https://"+target[i];
+		targetActive.location.href=domain;
+	}
+	i++;
+}
+setInterval(runRudyOpen,500);
